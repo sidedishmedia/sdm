b3dd8aa298a2ae2a97b8054d2bc5a4ade4910295
Raza Siddiqui <rezz.a.siddiqui604@gmail.com>
2020-01-06 21:56:10 +0500

    path test 2

--- staging/path.php
+++ staging/path.php
@@ -1,5 +1,6 @@
 <?php
-$dir = dirname(__FILE__);
-echo "<p>Full path to this dir: " . $dir . "</p>";
-echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+echo $rootPath
+$thisPath = dirname($_SERVER['PHP_SELF']);
+echo $thisPath
 ?>
